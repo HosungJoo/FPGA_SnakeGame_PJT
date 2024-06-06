bd4da410df90e3a432f58f4f4c2fd805b33f1d12
HosungJoo <136783996+HosungJoo@users.noreply.github.com>
2024-06-06 09:11:34 +0900

    Update apple.v

--- snake_proj.srcs/sources_1/imports/verilog/apple.v
+++ snake_proj.srcs/sources_1/imports/verilog/apple.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 
 module apple(
-	// 变量说明见top模块
+	
     input clock,
     input get_apple,
     input [1:0] game_status,
     
-    // 输出苹果坐标
+    
     output reg [5:0] apple_x,
     output reg [5:0] apple_y
     );
@@ -16,7 +16,7 @@ module apple(
     localparam DIE_FLASHING=2'b10;
     localparam INITIALIZING=2'b11;
     
-    // 用于随机（伪）生成苹果坐标
+   
     reg [11:0] random_for_x;
     reg [11:0] random_for_y;
 
@@ -41,7 +41,7 @@ module apple(
     	else
     	if (game_status==PLAYING && get_apple==1)
 		begin
-			// 防止苹果x和y坐标超范围
+			
 			apple_x<=(random_for_x[5:0]+1>46?(random_for_x[5:0]+1-20):(random_for_x[5:0]+1));
 			apple_y<=(random_for_y[4:0]+1>25?(random_for_y[4:0]+1-10):(random_for_y[4:0]+1));
 		end
